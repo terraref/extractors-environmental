1d3dfaaa1d00a2fd3cf7fd7f4f5c9f11dfc83dfe
FlyingWithJerome <jiarunm@uci.edu>
2017-03-12 17:24:08 -0700

    initial commit on geostream

--- environmentlogger/environmental_geostream.py
+++ environmentlogger/environmental_geostream.py
@@ -1,6 +1,7 @@
 import requests
 import subprocess
 import json
+import sys
 
 def _push_to_geostream(netCDF_handle, API_key):
     '''
@@ -10,23 +11,26 @@ def _push_to_geostream(netCDF_handle, API_key):
     *** Need support from bash/tcsh shell and NCO toolkits 4.6.2 and above (and their dependencies) ***
     *** The JSON dump of ncks is developed by Professor Charlie Zender and Mr. Henry Butowsky ***
     '''
-    _temporary_file_name = "temporary.json"
-    host_template = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?streamid=300&key={}".format(API_key)
+    host_get_template = "https://terraref.ncsa.illinois.edu/clowder/api/geostreams/datapoints?streamid=300&key={}".format(API_key)
+    host_post_template = ""
 
     try:
-        requests.get(host_template).raise_for_status()
+        requests.get(host_get_template).raise_for_status()
     except HTTPError as err:
         print err
         return -1
 
     ### Generate the JSON file with ncks ###
-    return_code = subprocess.call("ncks --json {} > {}",format(netCDF_handle, _temporary_file_name), shell=True)
+    # try:
+    raw_JSON = json.loads(subprocess.check_output(["ncks", "--json", netCDF_handle]))
+    # except CalledProcessError as err:
+    #     print err
+    #     return -1
 
-    if return_code == 1:
-        raise OSError("Invaild netCDF file handle directory")
-
-    with open(_temporary_file_name) as file_handle:
-        requests.post(host_template, json=json.dump(json.load(file_handle))) # weird, but using json.dump to make sure that the data is correctly encoded
-    
-    subprocess.call("rm "+_temporary_file_name) # remove the temporary JSON file
+    status = requests.post(host_get_template, json=json.dumps(raw_JSON)) # weird, but using json.dump to make sure that the data is correctly encoded
+    status.raise_for_status()
     return 0
+
+    
+if __name__ == '__main__':
+    _push_to_geostream(sys.argv[1], sys.argv[2])
