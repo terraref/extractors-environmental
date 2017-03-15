5d557b2af51338c0c33e3aaf786237c2259f1e0f
FlyingWithJerome <jiarunm@uci.edu>
2017-03-14 17:12:55 -0700

    the previous one is too verbose

--- environmentlogger/test.sh
+++ environmentlogger/test.sh
@@ -9,20 +9,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 END='\033[0m'
 
-has_python=$(module list | grep "python/2.7.10" | wc -m);
-has_gdal=$(module list | grep "gdal-stack/2.7.10" | wc -m);
-
 ##### Check if the account has Python and gdal-stack (which contains NumPy and netCDF4 and HDF5)
-if [ $has_python -eq 0 ];then
-    echo -e "${RED}Has no Python, try to load it...${END}"
-    module load python/2.7.10
-    echo -e "${GREEN}Python loading finished${END}";
-    if [ $has_gdal -eq 0 ];then
-        echo -e "${RED}Has no gdal-stack, try to load it...${END}"
-        module load gdal-stack/2.7.10
-        echo -e "${GREEN}gdal-stack loading finished${END}";
-    fi
-fi
+$(module list | grep "python/2.7.10" | wc -m) || module load python/2.7.10;
+$(module list | grep "gdal-stack/2.7.10" | wc -m) || module load gdal-stack/2.7.10;
 
 # Split the command into an array since it is tooooo looooooong
 # --For readability
@@ -39,11 +28,6 @@ python environmental_logger_json2netcdf.py
 .
 END
 )
-eval "${command[@]}" # Run the commands
-
-if [ $? -eq 0 ];then # Check if it returns with 0 (exit Successfully)
-    echo -e "${GREEN}Test Run Successfully Exited${END}";
-    rm 2016-10-15_19-56-57_environmentlogger.nc; # Clean up
-else
-    echo -e "${RED}Test Run Failed${END}";
-fi
+##### Run the command #####
+eval "${command[@]}" && echo -e "${GREEN}Test Run Successfully Exited${END}" && rm 2016-10-15_19-56-57_environmentlogger.nc\
+|| echo -e "${RED}Test Run Failed${END}";
