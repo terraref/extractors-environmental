ddd5a286d8d2db95f2b98955f327909224fc0f4c
Max Burnette <mburnet88@gmail.com>
2017-08-15 11:51:19 -0500

    Update energy farm extractor

--- energyfarm_datparser/terra_energyfarm_datparser.py
+++ energyfarm_datparser/terra_energyfarm_datparser.py
@@ -12,50 +12,20 @@ import pyclowder.datasets
 import terrautils.geostreams
 import terrautils.extractors
 
+from pyclowder.files import upload_metadata, download_metadata
+from terrautils.extractors import TerrarefExtractor, build_metadata
+from terrautils.geostreams import create_datapoint, create_sensor, create_stream, \
+	get_stream_by_name, get_sensor_by_name
+
 from parser import *
 
 
-class MetDATFileParser(Extractor):
+class MetDATFileParser(TerrarefExtractor):
 	def __init__(self):
-		Extractor.__init__(self)
-
-		influx_host = os.getenv("INFLUXDB_HOST", "terra-logging.ncsa.illinois.edu")
-		influx_port = os.getenv("INFLUXDB_PORT", 8086)
-		influx_db = os.getenv("INFLUXDB_DB", "extractor_db")
-		influx_user = os.getenv("INFLUXDB_USER", "terra")
-		influx_pass = os.getenv("INFLUXDB_PASSWORD", "")
-
-		# add any additional arguments to parser
-		self.parser.add_argument('--sensor', dest="sensor_name", type=str, nargs='?',
-								 default=('UIUC Energy Farm'),
-								 help="sensor name where streams and datapoints should be posted")
-		self.parser.add_argument('--influxHost', dest="influx_host", type=str, nargs='?',
-								 default=influx_host, help="InfluxDB URL for logging")
-		self.parser.add_argument('--influxPort', dest="influx_port", type=int, nargs='?',
-								 default=influx_port, help="InfluxDB port")
-		self.parser.add_argument('--influxUser', dest="influx_user", type=str, nargs='?',
-								 default=influx_user, help="InfluxDB username")
-		self.parser.add_argument('--influxPass', dest="influx_pass", type=str, nargs='?',
-								 default=influx_pass, help="InfluxDB password")
-		self.parser.add_argument('--influxDB', dest="influx_db", type=str, nargs='?',
-								 default=influx_db, help="InfluxDB database")
+		super(MetDATFileParser, self).__init__()
 
 		# parse command line and load default logging configuration
-		self.setup()
-
-		# setup logging for the exctractor
-		logging.getLogger('pyclowder').setLevel(logging.DEBUG)
-		logging.getLogger('__main__').setLevel(logging.DEBUG)
-
-		# assign other arguments
-		self.sensor_name = self.args.sensor_name
-		self.influx_params = {
-			"host": self.args.influx_host,
-			"port": self.args.influx_port,
-			"db": self.args.influx_db,
-			"user": self.args.influx_user,
-			"pass": self.args.influx_pass
-		}
+		self.setup(sensor='energyfarm_datparser')
 
 	def check_message(self, connector, host, secret_key, resource, parameters):
 		# Weather CEN_Avg15.dat, Weather CEN_DayAvg.dat
@@ -73,22 +43,21 @@ class MetDATFileParser(Extractor):
 		return CheckMessage.ignore
 
 	def process_message(self, connector, host, secret_key, resource, parameters):
-		starttime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-		created = 0
-		bytes = 0
+		self.start_message()
 
 		# TODO: Replace these with calls to Clowder fixed metadata
 		stream_name = 'Energy Farm Observations'
+		disp_name = self.sensors.get_display_name()
 		if 'Weather CEN' in resource['name']:
-			curr_sens = self.sensor_name + ' - CEN'
+			curr_sens = disp_name + ' - CEN'
 			stream_name+= ' CEN'
 			main_coords = [-88.199801,40.062051,0]
 		elif 'WeatherNE' in resource['name']:
-			curr_sens = self.sensor_name + ' - NE'
+			curr_sens = disp_name + ' - NE'
 			stream_name+= ' NE'
 			main_coords = [-88.193298,40.067379,0]
 		elif 'WeatherSE' in resource['name']:
-			curr_sens = self.sensor_name + ' - SE'
+			curr_sens = disp_name + ' - SE'
 			stream_name+= ' SE'
 			main_coords = [-88.193573,40.056910,0]
 		geom = {
@@ -97,9 +66,9 @@ class MetDATFileParser(Extractor):
 		}
 
 		# Get sensor or create if not found
-		sensor_data = terrautils.geostreams.get_sensor_by_name(connector, host, secret_key, curr_sens)
+		sensor_data = get_sensor_by_name(connector, host, secret_key, curr_sens)
 		if not sensor_data:
-			sensor_id = terrautils.geostreams.create_sensor(connector, host, secret_key, curr_sens, geom, {
+			sensor_id = create_sensor(connector, host, secret_key, curr_sens, geom, {
 					"id": "Met Station",
 					"title": "Met Station",
 					"sensorType": 4
@@ -108,15 +77,14 @@ class MetDATFileParser(Extractor):
 			sensor_id = sensor_data['id']
 
 		# Get stream or create if not found
-		stream_data = terrautils.geostreams.get_stream_by_name(connector, host, secret_key, stream_name)
+		stream_data = get_stream_by_name(connector, host, secret_key, stream_name)
 		if not stream_data:
-			stream_id = terrautils.geostreams.create_stream(connector, host, secret_key, stream_name, sensor_id, geom)
+			stream_id = create_stream(connector, host, secret_key, stream_name, sensor_id, geom)
 		else:
 			stream_id = stream_data['id']
 
-
 		# Get metadata to check till what time the file was processed last. Start processing the file after this time
-		allmd = pyclowder.files.download_metadata(connector, host, secret_key, resource['id'])
+		allmd = download_metadata(connector, host, secret_key, resource['id'])
 		last_processed_time = 0
 		datapoint_count = 0
 		for md in allmd:
@@ -127,7 +95,6 @@ class MetDATFileParser(Extractor):
 				else:
 					datapoint_count = 0
 				delete_metadata(connector, host, secret_key, resource['id'], md['agent']['name'].split("/")[-1])
-
 
 		# Parse file and get all the records in it.
 		ISO_8601_UTC_OFFSET = dateutil.tz.tzoffset("-07:00", -7 * 60 * 60)
@@ -140,21 +107,19 @@ class MetDATFileParser(Extractor):
 		dp = 0
 		for record in records:
 			try:
-				terrautils.geostreams.create_datapoint(connector, host, secret_key, stream_id, record['geometry'],
-												  record['start_time'], record['end_time'], record['properties'])
+				create_datapoint(connector, host, secret_key, stream_id, record['geometry'],
+								record['start_time'], record['end_time'], record['properties'])
 				dp += 1
 			except:
 				logging.error("error creating datapoint at "+record['start_time'])
 
 		# Mark dataset as processed
-		metadata = terrautils.extractors.build_metadata(host, self.extractor_info['name'], resource['id'], {
+		metadata = build_metadata(host, self.extractor_info['name'], resource['id'], {
 			"last processed time": records[-1]["end_time"],
 			"datapoints_created": datapoint_count + dp}, 'file')
-		pyclowder.files.upload_metadata(connector, host, secret_key, resource['id'], metadata)
+		upload_metadata(connector, host, secret_key, resource['id'], metadata)
 
-		endtime = datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
-		terrautils.extractors.log_to_influxdb(self.extractor_info['name'], self.influx_params,
-											  starttime, endtime, created, bytes)
+		self.end_message()
 
 def delete_metadata(connector, host, key, fileid, extractor=None):
     """Delete file JSON-LD metadata from Clowder.
