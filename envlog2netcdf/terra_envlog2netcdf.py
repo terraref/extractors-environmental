a6ec553395a5826a824ec22d80ad42b97ee25b47
Max Burnette <mburnet2@illinois.com>
2019-03-27 08:55:56 -0500

    write datapoints to CSV

--- envlog2netcdf/terra_envlog2netcdf.py
+++ envlog2netcdf/terra_envlog2netcdf.py
@@ -2,18 +2,16 @@
 
 import datetime
 import os
-import logging
 import shutil
 import subprocess
+import json
 from netCDF4 import Dataset
 
 from pyclowder.utils import CheckMessage
 from pyclowder.datasets import get_info, get_file_list, download_metadata, upload_metadata
-from pyclowder.files import upload_to_dataset
-from terrautils.extractors import TerrarefExtractor, build_dataset_hierarchy, build_metadata, \
+from pyclowder.files import upload_to_dataset, submit_extraction
+from terrautils.extractors import TerrarefExtractor, build_dataset_hierarchy_crawl, build_metadata, \
     is_latest_file, file_exists, contains_required_files
-from terrautils.geostreams import get_sensor_by_name, create_datapoints, create_stream, \
-    create_sensor, get_stream_by_name
 from terrautils.metadata import get_extractor_metadata
 
 import environmental_logger_json2netcdf as ela
@@ -24,11 +22,14 @@ def add_local_arguments(parser):
     parser.add_argument('--batchsize', type=int, default=3000,
                         help="max number of datapoints to submit at a time")
 
-def get_hour(filename):
-    timestamp = filename.split('_')[1]
-    hour = timestamp.split('-')[0]
-    return int(hour)
+def _produce_attr_dict(netCDF_variable_obj):
+    '''
+    Produce a list of dictionary with attributes and value (Each dictionary is one datapoint)
+    '''
+    attributes = [attr for attr in dir(netCDF_variable_obj) if isinstance(attr, unicode)]
+    result     = {name:getattr(netCDF_variable_obj, name) for name in attributes}
 
+    return [dict(result.items()+ {"value":str(data)}.items()) for data in netCDF_variable_obj[...]]
 
 class EnvironmentLoggerJSON2NetCDF(TerrarefExtractor):
     def __init__(self):
@@ -50,7 +51,7 @@ class EnvironmentLoggerJSON2NetCDF(TerrarefExtractor):
             self.log_skip(resource, "not latest file")
             return CheckMessage.ignore
 
-        if len(resource['files'] >= 23):
+        if len(resource['files']) >= 23:
             md = download_metadata(connector, host, secret_key, resource['id'])
             if get_extractor_metadata(md, self.extractor_info['name'], self.extractor_info['version']):
                 timestamp = resource['name'].split(" - ")[1]
@@ -70,21 +71,24 @@ class EnvironmentLoggerJSON2NetCDF(TerrarefExtractor):
         json_files = []
         for f in resource['files']:
             if f['filename'].endswith("_environmentlogger.json"):
-                json_files.append(f['filepath'])
+                if f['filepath'].startswith("/home/clowder"):
+                    json_files.append(f['filepath'].replace("/home/clowder", "/home/extractor"))
+                else:
+                    json_files.append(f['filepath'])
         json_files.sort()
 
         # Determine full output path
         timestamp = resource['name'].split(" - ")[1]
         out_fullday_netcdf = self.sensors.create_sensor_path(timestamp)
+        temp_out_full = os.path.join(os.path.dirname(out_fullday_netcdf), "temp_full.nc")
+        temp_out_single = temp_out_full.replace("_full.nc", "_single.nc")
 
-        temp_out_full = "temp_full.nc"
-        temp_out = "temp.nc"
         for json_file in json_files:
-            self.log_info(resource, "converting %s to netCDF & appending" % json_file)
-            ela.mainProgramTrigger(json_file, temp_out)
-            cmd = "ncrcat --record_append %s %s" % (temp_out, temp_out_full)
+            self.log_info(resource, "converting %s to netCDF & appending" % os.path.basename(json_file))
+            ela.mainProgramTrigger(json_file, temp_out_single)
+            cmd = "ncrcat --record_append %s %s" % (temp_out_single, temp_out_full)
             subprocess.call([cmd], shell=True)
-            os.remove(temp_out)
+            os.remove(temp_out_single)
 
         shutil.move(temp_out_full, out_fullday_netcdf)
         self.created += 1
@@ -92,20 +96,55 @@ class EnvironmentLoggerJSON2NetCDF(TerrarefExtractor):
 
         # Write out geostreams.csv
         self.log_info(resource, "writing geostreams CSV")
-        prepareDatapoint(connector, host, secret_key, resource, out_fullday_netcdf, self.batchsize)
+        geo_csv = out_fullday_netcdf.replace(".nc", "_geo.csv")
+        geo_file = open(geo_csv, 'w')
+        geo_file.write(','.join(['site', 'trait', 'lat', 'lon', 'dp_time', 'source', 'value', 'timestamp']) + '\n')
+        with Dataset(out_fullday_netcdf, "r") as ncdf:
+            streams = set([sensor_info.name for sensor_info in ncdf.variables.values() if sensor_info.name.startswith('sensor')])
+            for stream in streams:
+                if stream != "sensor_spectrum":
+                    try:
+                        memberlist = ncdf.get_variables_by_attributes(sensor=stream)
+                        for members in memberlist:
+                            data_points = _produce_attr_dict(members)
+                            for index in range(len(data_points)):
+                                dp_obj = data_points[index]
+                                if dp_obj["sensor"] == stream:
+                                    time_format = "%Y-%m-%dT%H:%M:%S-07:00"
+                                    time_point = (datetime.datetime(year=1970, month=1, day=1) + \
+                                                  datetime.timedelta(days=ncdf.variables["time"][index])).strftime(time_format)
+
+                                    geo_file.write(','.join(["Full Field - Environmental Logger",
+                                                             "(EL) %s" % stream,
+                                                             str(33.075576),
+                                                             str(-111.974304),
+                                                             time_point,
+                                                             host + ("" if host.endswith("/") else "/") + "datasets/" + resource['id'],
+                                                             '"%s"' % json.dumps(dp_obj).replace('"', '""'),
+                                                             timestamp]) + '\n')
+
+                    except:
+                        self.log_error(resource, "NetCDF attribute not found: %s" % stream)
 
         # Fetch dataset ID by dataset name if not provided
-        target_dsid = build_dataset_hierarchy(host, secret_key, self.clowder_user, self.clowder_pass, self.clowderspace,
-                                  self.sensors.get_display_name(), timestamp[:4], timestamp[5:7],
-                                  leaf_ds_name=self.sensors.get_display_name()+' - '+timestamp)
+        target_dsid = build_dataset_hierarchy_crawl(host, secret_key, self.clowder_user, self.clowder_pass, self.clowderspace,
+                                                    None, None, self.sensors.get_display_name(),
+                                                    timestamp[:4], timestamp[5:7], timestamp[8:10],
+                                                    leaf_ds_name=self.sensors.get_display_name() + ' - ' + timestamp)
         ds_files = get_file_list(connector, host, secret_key, target_dsid)
         found_full = False
-
+        found_csv  = False
         for f in ds_files:
             if f['filename'] == os.path.basename(out_fullday_netcdf):
                 found_full = True
+            if f['filename'] == os.path.basename(geo_csv):
+                found_csv = True
         if not found_full:
             upload_to_dataset(connector, host, secret_key, target_dsid, out_fullday_netcdf)
+        if not found_csv:
+            geoid = upload_to_dataset(connector, host, secret_key, target_dsid, geo_csv)
+            self.log_info(resource, "triggering geostreams extractor on %s" % geoid)
+            submit_extraction(connector, host, secret_key, geoid, "terra.geostreams")
 
         # Tell Clowder this is completed so subsequent file updates don't daisy-chain
         ext_meta = build_metadata(host, self.extractor_info, resource['id'], {
@@ -114,73 +153,6 @@ class EnvironmentLoggerJSON2NetCDF(TerrarefExtractor):
         upload_metadata(connector, host, secret_key, resource['id'], ext_meta)
 
         self.end_message(resource)
-
-
-def _produce_attr_dict(netCDF_variable_obj):
-    '''
-    Produce a list of dictionary with attributes and value (Each dictionary is one datapoint)
-    '''
-    attributes = [attr for attr in dir(netCDF_variable_obj) if isinstance(attr, unicode)]
-    result     = {name:getattr(netCDF_variable_obj, name) for name in attributes}
-
-    return [dict(result.items()+ {"value":str(data)}.items()) for data in netCDF_variable_obj[...]]
-
-def prepareDatapoint(connector, host, secret_key, resource, ncdf, batchsize):
-    # TODO: Get this from Clowder
-    coords = [-111.974304, 33.075576, 0]
-    geom = {
-        "type": "Point",
-        "coordinates": coords
-    }
-
-    with Dataset(ncdf, "r") as netCDF_handle:
-        sensor_name = "Full Field - Environmental Logger"
-        sensor_data = get_sensor_by_name(connector, host, secret_key, sensor_name)
-        if not sensor_data:
-            sensor_id = create_sensor(connector, host, secret_key, sensor_name, geom,
-                                      {"id": "MAC Field Scanner", "title": "MAC Field Scanner", "sensorType": 4},
-                                      "Maricopa")
-        else:
-            sensor_id = sensor_data['id']
-
-        stream_list = set([sensor_info.name for sensor_info in netCDF_handle.variables.values() if sensor_info.name.startswith('sensor')])
-        for stream in stream_list:
-            if stream != "sensor_spectrum":
-                stream_name = "(EL) %s" % stream
-                stream_data = get_stream_by_name(connector, host, secret_key, stream_name)
-                if not stream_data:
-                    stream_id = create_stream(connector, host, secret_key, stream_name, sensor_id, geom)
-                else:
-                    stream_id = stream_data['id']
-
-                try:
-                    memberlist = netCDF_handle.get_variables_by_attributes(sensor=stream)
-                    for members in memberlist:
-                        data_points = _produce_attr_dict(members)
-                        data_point_list = []
-                        for index in range(len(data_points)):
-                            dp_obj = data_points[index]
-                            if dp_obj["sensor"] == stream:
-                                time_format = "%Y-%m-%dT%H:%M:%S-07:00"
-                                time_point = (datetime.datetime(year=1970, month=1, day=1) + \
-                                              datetime.timedelta(days=netCDF_handle.variables["time"][index])).strftime(time_format)
-
-                                data_point_list.append({
-                                    "start_time": time_point,
-                                    "end_time": time_point,
-                                    "type": "Point",
-                                    "geometry": geom,
-                                    "properties": dp_obj
-                                })
-
-                                if len(data_point_list) > batchsize:
-                                    create_datapoints(connector, host, secret_key, stream_id, data_point_list)
-                                    data_point_list = []
-
-                        if len(data_point_list) > 0:
-                            create_datapoints(connector, host, secret_key, stream_id, data_point_list)
-                except:
-                    logging.error("NetCDF attribute not found: %s" % stream)
 
 if __name__ == "__main__":
     extractor = EnvironmentLoggerJSON2NetCDF()
